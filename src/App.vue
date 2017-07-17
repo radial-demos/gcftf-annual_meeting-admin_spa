d8821c59abe9725ea1473ca84a5e3365fcd4f867
Darren Anderson <dranders@colorado.edu>
2017-07-17 16:27:52 -0600

    Remove unnecessary code.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <v-app>
     <v-toolbar>
       <v-toolbar-title>Annual Meeting Registrants</v-toolbar-title>
-      <!-- <v-spacer></v-spacer> -->
     </v-toolbar>
     <main>
       <v-container>
@@ -14,32 +13,7 @@
     </v-footer>
   </v-app>
 </template>
-<script>
-
-  export default {
-    data () {
-      return {
-        title: '2017 GCF Annual Meeting',
-      }
-    },
-    // created () {
-    //   console.log('LISTO');
-    // },
-    methods: {
-      setActiveLang: function(lang) {
-        this.activeLang = lang;
-        this.activeTranslationLookup = translationLookups[this.activeLang];
-      },
-    }
-  }
-</script>
 
 <style lang="stylus">
   @import './stylus/main'
-  main {
-    /*background-color: #94a9a4;*/
-  }
-  main > .container {
-    /*max-width: 800px;*/
-  }
 </style>
