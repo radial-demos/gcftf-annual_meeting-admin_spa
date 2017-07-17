e0c7c2ebdb26818b68b6ac5cb9bf891381cf4c80
Darren Anderson <dranders@colorado.edu>
2017-07-17 10:59:44 -0600

    Include RegistrantDetail component.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import ListingVue from './pages/Listing.vue';
 import RegistrantDetailVue from './RegistrantDetail.vue';
+import TempVue from './Temp.vue';
 
 Vue.use(Vuetify);
 
 Vue.component('listing-page', ListingVue);
 Vue.component('registrant-detail', RegistrantDetailVue);
+Vue.component('temp', TempVue);
 
 new Vue({
   el: '#app',
