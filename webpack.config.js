55c6930e8cceefe746087019550b94b1605bde4d
Darren Anderson <dranders@colorado.edu>
2017-10-16 11:54:17 -0700

    Cleanup linting errors.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   resolve: {
     alias: {
-      'public': path.resolve(__dirname, './public')
-    }
+      public: path.resolve(__dirname, './public'),
+    },
   },
   module: {
     rules: [
@@ -23,11 +23,11 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
           // other vue-loader options go here
-        }
+        },
       },
       {
         test: /\.js$/,
